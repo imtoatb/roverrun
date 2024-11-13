00c73d604b7aa91d24bbc9775d8cd0d9da65617e
imtoatb <idfc.gfy@gmail.com>
2024-11-13 19:20:14 +0100

    Trying to position the robot

--- test.c
+++ test.c
@@ -3,6 +3,11 @@
 #include "loc.c"
 #include "loc.h"
 
+
+#define x_max 7
+#define y_max 6
+
+
 //To visualize the robot
 char robot(t_position loc, int x_max, int y_max) {
     while(isValidLocalisation(loc, x_max, y_max) == 0) {
@@ -11,6 +16,9 @@ char robot(t_position loc, int x_max, int y_max) {
     return "good";
 }
 
-void updateMap(char map[x_max][y_max],t_position robot_pos) {
-
+void updateMap(char map[x_max][y_max], t_position robot_pos) {
+    // Vérifier que la position du robot est valide
+    if (isValidLocalisation(robot_pos, x_max, y_max)) {
+        map[robot_pos.y][robot_pos.x] = 'R';  // 'R' représente la position du robot
+    }
 }
