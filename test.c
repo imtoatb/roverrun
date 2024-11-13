8a6f8616fcd37e5f382692d505abf659cb8dc8e3
imtoatb <idfc.gfy@gmail.com>
2024-11-13 19:25:21 +0100

    Trying to position the robot

--- test.c
+++ test.c
@@ -8,17 +8,15 @@
 #define y_max 6
 
 
-//To visualize the robot
-char robot(t_position loc, int x_max, int y_max) {
-    while(isValidLocalisation(loc, x_max, y_max) == 0) {
-        printf("The robot is outside the map. OVER");
-    }
-    return "good";
+void initMap(char map[x_max][y_max]){
+
+    t_map map = createMapFromFile("..\\maps\\example1.map");
 }
 
-void updateMap(char map[x_max][y_max], t_position robot_pos) {
-    // Vérifier que la position du robot est valide
-    if (isValidLocalisation(robot_pos, x_max, y_max)) {
+void updateMap(char map[x_max][y_max], t_position robot_pos){
+
+    if (isValidLocalisation(robot_pos, x_max, y_max)){
         map[robot_pos.y][robot_pos.x] = 'R';  // 'R' représente la position du robot
+
     }
 }
--- test.h
+++ test.h
@@ -1,7 +1,14 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "loc.h"
 #include "main.c"
 #include "test.c"
 
-void posRobot(loc, x_max, y_max)
+#define x_max 7
+#define y_max 6
+
+void initMap(char map[x_max][y_max]);
 
 void updateMap()
 
