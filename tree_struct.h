d6643f32b79fedc59716802361e8841a8fd458d3
imtoatb <idfc.gfy@gmail.com>
2024-11-14 08:34:03 +0100

    Trying to position the robot

---
+++ tree_struct.h
@@ -0,0 +1,31 @@
+#include "loc.h"
+#include "moves.h"
+
+#ifndef TREE_STRUCT_H
+#define TREE_STRUCT_H
+
+
+typedef struct s_node {
+    struct s_node **children;
+    struct s_node *parent;
+    t_position localisation;
+    int cost;
+    int possibilities;
+} t_node;
+
+typedef struct s_root {
+    struct s_node **children;
+    int cost;
+} t_root;
+
+typedef struct s_tree {
+    t_root *tree;
+} t_tree;
+
+t_node* create_node(t_position localisation, t_move move, int cost);
+
+void free_node(t_node* node);
+
+voide add_child(t_node* parent, t_node* child);
+
+#endif //TREE_STRUCT_H
