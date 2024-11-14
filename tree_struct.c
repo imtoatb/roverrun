4a37b4a893dcb5690579452915112677591a183a
imtoatb <idfc.gfy@gmail.com>
2024-11-14 08:52:55 +0100

    Working on the tree, function add_child ok, free_node ok, free_tree now

--- tree_struct.c
+++ tree_struct.c
@@ -33,13 +33,34 @@ void add_child(t_node* parent, t_node* child) {
     if (!parent->children) return;
 
 
-    parent->children[parent->possibilities] = child; //+1 child
+    parent->children[parent->possibilities] = child;                //+1 child
     parent->possibilities++;
 }
 
 void free_node(t_node* node) {
+    if (!node) return;
 
+    for (int i = 0; i < node->possibilities; i++) {                 //Free all children
+        free_node(node->children[i]);
+    }
+
+    free(node->children);
+    free(node);
 }
 
+
+void free_tree(t_tree* tree) {
+    if (!tree) return;
+
+
+    if (tree->tree && tree->tree->children) {
+        free_node(tree->tree->children[0]);
+    }
+
+
+    free(tree->tree->children);
+    free(tree->tree);
+    free(tree);
+}
 
 
